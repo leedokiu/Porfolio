c22eb9c59b9805341b636e84b5e49f5a5f48ff11
leedokiu <72475518+leedokiu@users.noreply.github.com>
2025-09-09 10:05:50 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import PortfolioNeumorphism from './PortfolioNeumorphism'
 import './index.css'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PortfolioNeumorphism />
-  </React.StrictMode>,
+  <React.StrictMode><PortfolioNeumorphism /></React.StrictMode>
 )
